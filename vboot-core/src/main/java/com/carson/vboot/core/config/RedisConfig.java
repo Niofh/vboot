d1b24f398d6c75fdf8ec0656d88a1a33b63abdde
Nicofh <598220654@qq.com>
2020-03-12 22:24:21 +0800

    redis配置加入缓存时间

--- vboot-core/src/main/java/com/carson/vboot/core/config/RedisConfig.java
+++ vboot-core/src/main/java/com/carson/vboot/core/config/RedisConfig.java
@@ -4,31 +4,50 @@ package com.carson.vboot.core.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.cache.Cache;
 import org.springframework.cache.CacheManager;
 import org.springframework.cache.annotation.CachingConfigurerSupport;
 import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cache.interceptor.CacheErrorHandler;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
 import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.RedisSerializer;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+import java.time.Duration;
 
 /**
  * redis json格式化
  */
 @EnableCaching
 @Configuration
+@Slf4j
+@ConfigurationProperties(prefix = "redis")
 public class RedisConfig extends CachingConfigurerSupport {
 
-//    private Duration timeToLive = Duration.ZERO;
-//
-//    public void setTimeToLive(Duration timeToLive) {
-//        this.timeToLive = timeToLive;
-//    }
+    private String unit;
+    private Integer time;
+
+    /**
+     * RedisTemplate 格式化配置
+     */
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> template = new RedisTemplate<>();
+        template.setConnectionFactory(redisConnectionFactory);
+        Jackson2JsonRedisSerializer jackson2JsonRedisSerializer = new Jackson2JsonRedisSerializer<Object>(Object.class);
+        // 设置json序列化格式
+        template.setDefaultSerializer(jackson2JsonRedisSerializer);
+        return template;
+    }
 
     @Bean
     public CacheManager cacheManager(RedisConnectionFactory factory) {
@@ -38,12 +57,25 @@ public class RedisConfig extends CachingConfigurerSupport {
         //解决查询缓存转换异常的问题
         ObjectMapper om = new ObjectMapper();
         om.setVisibility(PropertyAccessor.ALL, JsonAutoDetect.Visibility.ANY);
-        om.enableDefaultTyping(ObjectMapper.DefaultTyping.NON_FINAL);
+        om.activateDefaultTyping(new ObjectMapper().getPolymorphicTypeValidator(), ObjectMapper.DefaultTyping.NON_FINAL);
         jackson2JsonRedisSerializer.setObjectMapper(om);
+
+        if (time < 0) {
+            time = -1;
+        }
+        Duration expireTime;
+        if ("hour".equals(unit)) {
+            expireTime = Duration.ofHours(time); // 多少小时
+        } else if ("minute".equals(unit)) {
+            expireTime = Duration.ofMinutes(time); // 多少分钟
+        } else {
+            expireTime = Duration.ofDays(time);  // 多少天
+        }
+
 
         // 配置序列化（解决乱码的问题）
         RedisCacheConfiguration config = RedisCacheConfiguration.defaultCacheConfig()
-                //.entryTtl(timeToLive)
+                .entryTtl(expireTime) // 缓存时间
                 .serializeKeysWith(RedisSerializationContext.SerializationPair.fromSerializer(redisSerializer))
                 .serializeValuesWith(RedisSerializationContext.SerializationPair.fromSerializer(jackson2JsonRedisSerializer))
                 .disableCachingNullValues();
@@ -52,5 +84,41 @@ public class RedisConfig extends CachingConfigurerSupport {
                 .cacheDefaults(config)
                 .build();
         return cacheManager;
+    }
+
+    /**
+     * 异常处理 当Redis缓存相关操作发生异常时 打印日志 程序正常走
+     *
+     * @return
+     */
+    @Override
+    public CacheErrorHandler errorHandler() {
+
+        CacheErrorHandler cacheErrorHandler = new CacheErrorHandler() {
+            @Override
+            public void handleCacheGetError(RuntimeException e, Cache cache, Object key) {
+                log.error("{}", e);
+                log.warn("Redis occur handleCacheGetError：key: [{}]", key);
+            }
+
+            @Override
+            public void handleCachePutError(RuntimeException e, Cache cache, Object key, Object value) {
+                log.error("{}", e);
+                log.warn("Redis occur handleCachePutError：key: [{}]；value: [{}]", key, value);
+            }
+
+            @Override
+            public void handleCacheEvictError(RuntimeException e, Cache cache, Object key) {
+                log.error("{}", e);
+                log.warn("Redis occur handleCacheEvictError：key: [{}]", key);
+            }
+
+            @Override
+            public void handleCacheClearError(RuntimeException e, Cache cache) {
+                log.error("{}", e);
+                log.warn("Redis occur handleCacheClearError");
+            }
+        };
+        return cacheErrorHandler;
     }
 }
