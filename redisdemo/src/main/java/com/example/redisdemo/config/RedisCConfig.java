1b3989265a3e2d82c02ee36461f6551717841dcf
ofh <l>
2020-08-07 15:01:40 +0800

    fix:增加redis功能

--- redisdemo/src/main/java/com/example/redisdemo/config/RedisCConfig.java
+++ redisdemo/src/main/java/com/example/redisdemo/config/RedisCConfig.java
@@ -19,7 +19,6 @@ import org.springframework.data.redis.core.ListOperations;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.listener.RedisMessageListenerContainer;
 import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
-import org.springframework.data.redis.serializer.RedisSerializationContext;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 /**
